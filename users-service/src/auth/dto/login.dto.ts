483dbc076bbec678cf5f289ccb965df4c7b3a33f
milenaa052 <milenasantosdeoliveira40@gmail.com>
2025-11-06 18:02:18 -0300

    refactor: fixing login error messages

--- users-service/src/auth/dto/login.dto.ts
+++ users-service/src/auth/dto/login.dto.ts
@@ -1,11 +1,9 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-    @IsEmail()
-    @IsNotEmpty()
+    @IsEmail({}, { message: 'Email inválido' })
     email: string;
 
     @IsString()
-    @IsNotEmpty()
     password: string;
 }
